21562a29beb01e9a83f85b4495faeb392fa3e8a4
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2022-09-05 09:33:51 +0200

    setup material ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+	import Button from '@smui/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button on:click={() => alert('Primary button clicked')}>Primary</Button>
+<Button on:click={() => alert('Secondary button clicked')} color="secondary">Secondary</Button>
