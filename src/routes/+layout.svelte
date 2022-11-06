3ebb59aadb3de2136051bcb291a776bd79379a1d
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2022-11-06 19:09:41 +0100

    add blueprint pattern

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,38 @@
-<svelte:window on:resize={setMiniWindow} />
-<svelte:head>
-  <link rel="stylesheet" href="{assets}/styles/main.css" />
-</svelte:head>
-
 <script>
-  import { assets } from '$app/paths';
+  import { assets } from "$app/paths";
 
   import "../main.css"; // import tailwindcss
-  import { onMount } from 'svelte';
-  
+  import { onMount } from "svelte";
+
   // layout
-	import DashboardHeader from '$lib/components/layout/dashboard-header.svelte'
-	import DashboardSidebar from '$lib/components/layout/dashboard-sidebar.svelte'
+  import DashboardHeader from "$lib/components/layout/dashboard-header.svelte";
+  import DashboardSidebar from "$lib/components/layout/dashboard-sidebar.svelte";
 
   // functionality
   let innerWidth = 0;
   let miniWindow = false;
 
   onMount(setMiniWindow);
-  
+
   function setMiniWindow() {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       innerWidth = window.innerWidth;
       miniWindow = innerWidth < 768;
     }
   }
 </script>
 
+<svelte:window on:resize={setMiniWindow} />
+<svelte:head>
+  <link rel="stylesheet" href="{assets}/styles/main.css" />
+</svelte:head>
+
 <div class="dashboard-wrapper">
   <DashboardSidebar propIsMiniWindow={miniWindow} />
-  
+
   <div class="dashboard-content">
     <DashboardHeader />
-    
+
     <main class="dashboard-main">
       <div class="container h-full">
         <div class="inner">
@@ -40,14 +40,15 @@
           <p>Width: {innerWidth}</p>
           <p>
             Fixed Sidebar:
-            {#if miniWindow} Closed
-            {:else} Open
+            {#if miniWindow}
+              Closed
+            {:else}
+              Open
             {/if}
           </p>
         </div>
       </div>
     </main>
-
   </div>
 </div>
 
@@ -60,11 +61,20 @@
     flex: 1;
     position: relative;
     padding-top: var(--dashboard-header-height);
-	  transition: padding-left var(--transition-duration) var(--transition-easing);
+    transition: padding-left var(--transition-duration) var(--transition-easing);
   }
-@media (min-width: 768px) {
-	.dashboard-content {
-		padding-left: 5rem;
-	}
-}
+  @media (min-width: 768px) {
+    .dashboard-content {
+      padding-left: 5rem;
+      background-image: linear-gradient(
+          rgba(0, 0, 0, 0.02) 2px,
+          transparent 2px
+        ),
+        linear-gradient(90deg, rgba(0, 0, 0, 0.02) 2px, transparent 1px),
+        linear-gradient(rgba(0, 0, 0, 0.04) 1px, transparent 1px),
+        linear-gradient(90deg, rgba(0, 0, 0, 0.04) 1px, transparent 1px);
+      background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
+      background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
+    }
+  }
 </style>
